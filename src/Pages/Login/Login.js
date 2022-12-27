fd741c2784a63dc896915f0f1d0310a0d1b7d9e5
Sujoy Paul <freelancersujoy@gmail.com>
2022-12-27 22:28:54 +0600

    login with email pass or google

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { FaGoogle } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider';
 
 const Login = () => {
+  const navigate = useNavigate()
+  const {user, singIn, googleSign} = useContext(AuthContext);
+  const [error, setError] = useState('');
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    singIn(email, password)
+    .then(result => {
+      const user = result.user;
+      navigate('/')
+      setError('')
+    })
+    .catch(err => {
+      setError(err.message)
+      console.log(err);
+    })
+  }
+
+  const handlenGoogleLogin = () => {
+    googleSign()
+    .then(() => {})
+    .catch(err => setError(err.message))
+  }
+
     return (
-        <form className='mt-10 mb-40'>
+        <form onSubmit={handleSignIn} className='mt-10 mb-40'>
             <div className="hero">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className=" lg:text-left">
@@ -17,18 +45,21 @@ const Login = () => {
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input required name='email' type="text" placeholder="email" className="input input-bordered" />
         </div>
+
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
+          <input required name='password' type="password" placeholder="password" className="input input-bordered" />
         </div>
+
         <div className="flex flex-col w-full border-opacity-50">
             <div className="grid">
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          {error && <p className='mb-3 text-error'>{error}</p>}
+          <button type='submit' className="btn btn-primary">Login</button>
         </div>
                
             </div>
